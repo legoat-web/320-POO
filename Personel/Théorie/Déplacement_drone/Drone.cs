daa242bd8beca9f3fa38d34fd953137b85376076
Giulian desvaux <gidesvaux@gmail.com>
2026-09-04 12:12:57 +0200

    Test

---
+++ Personel/Théorie/Déplacement_drone/Drone.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Déplacement_drone
+{
+    internal class Drone
+    {
+        private string _modelA = "x-0-x";
+        private string _modelD = "_____";
+        private int _posx;
+        private int _posy;
+        public int _battery;
+        
+        public Drone(int posx, int posy, int battery)
+        {
+            _posx = posx;
+            _posy = posy;
+            _battery = battery;
+        }
+
+        public void ChangeState(ref int _posx, ref int _battery)
+        {
+            _posx += 1;
+            _battery -= 2;
+        }
+
+        public void Draw(int _posx, int _posy, int _battery, string _modelA, string _modelD)
+        {
+            Console.Clear();
+            Console.SetCursorPosition(_posx, _posy);
+
+            if (_battery > 0)
+                Console.Write(_modelA);
+
+            else  
+                Console.Write(_modelD);
+        
+            Console.SetCursorPosition(_posx +1, _posy -1);
+
+            if(_battery > 0)
+                Console.Write($"{_battery}%");
+
+            Thread.Sleep(200);
+        }
+
+        public void Ending()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(_posx, _posy);
+            Console.Write("MORT");
+            Thread.Sleep(1000);
+        }
+
+    
+        public void Execute()
+        {
+            while(_battery > 0)
+            {
+                ChangeState(ref _posx, ref _battery);
+                Draw(_posx, _posy, _battery, _modelA, _modelD);
+            }
+            Ending();
+        }
+    }
+}
+
+
+//}
+
+//    
+//
+
+//static void Ending(ref string drone, int posx1, int posy1)
+//{
+//    Console.Clear();
+//    Console.SetCursorPosition(posx1, posy1);
+//    drone = "____";
+//    Console.Write(drone);
+
+//    Thread.Sleep(500);
+//    Console.SetCursorPosition(posx1, posy1);
+//    Console.ForegroundColor = ConsoleColor.Red;
+//    Console.WriteLine("/!\\ MORT /!\\");
+//    Console.ResetColor();
--- Personel/Théorie/Déplacement_drone/Program.cs
+++ Personel/Théorie/Déplacement_drone/Program.cs
@@ -12,64 +12,14 @@ namespace Déplacement_drone
     {
         static async Task Main(string[] args)
         {
-            const int delay = 100;
-            const int posy1 = 10;
-            const int posy2 = 20;
+            Console.CursorVisible = false;
 
-            int posx1 = 0;
-            int posx2 = 0;
-
-            var battery1 = 50;
-            var battery2 = 50;
-
-            string drone = "x-0-x";
-
-            while (battery1 > 0)
+            Drone[] drones =
             {
-                await Task.Delay(delay);
-                Movedrone(delay, ref posx1);
-                Movedrone(delay, ref posx2);
+                 new Drone(0, 20, 50),
+                 new Drone(3, 6, 70)
+            };
 
-                Removebattery1(ref battery1);
-                Removebattery1(ref battery2);
-
-                Show(drone, posx1, posy1);
-                Show(drone, posx2, posy2);
-            }
-            Ending(ref drone, posx1, posy1);
-            Ending(ref drone, posx2, posy2); // A VERIFIER CAR LA FIN NE MARCHE PAS
-        }
-
-        static void Movedrone(int delay, ref int posx1)
-        {
-            Console.Clear();
-            posx1 += 1;
-        }
-
-        static void Removebattery1(ref int battery1)
-        {
-            battery1 -= 2;
-        }
-        static void Show(string drone, int posx1, int posy1)
-        {
-            Console.SetCursorPosition(posx1, posy1);
-            Console.Write(drone);
-        }
-
-        static void Ending(ref string drone, int posx1, int posy1)
-        {
-            Console.Clear();
-            Console.SetCursorPosition(posx1, posy1);
-            drone = "____";
-            Console.Write(drone);
-
-            Thread.Sleep(500);
-            Console.SetCursorPosition(posx1, posy1);
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("/!\\ MORT /!\\");
-            Console.ResetColor();
-
-            Console.ReadKey();
         }
     }
 }
